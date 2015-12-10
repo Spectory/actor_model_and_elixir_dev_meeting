1e83b925324538e076292cee346175239b588a93
guy <guy@spectory.com>
2015-12-10 12:48:54 +0200

    monsters

--- iex/demo.exs
+++ iex/demo.exs
@@ -9,16 +9,40 @@ defmodule CookieMonster do
   end
 
   defp eat_cookie({:food, nil}) do
-    IO.puts "Hey! where is my cookie you bastard?!"
+    print_red "Hey! where is my cookie you bastard?!"
   end
   defp eat_cookie({:food, "cookie"}) do
-    IO.puts "Om Num Num Num Num!"
+    print_green "Om Num Num Num Num!"
   end
   defp eat_cookie({:food, "broccoli"}) do
-    IO.puts "I hate broccoli!"
+    print_red "I hate broccoli!"
   end
   defp eat_cookie({:food, somthing}) do
-    IO.puts "#{somthing}?? WTF is that?!"
+    print_red "#{somthing}?? WTF is that?!"
+  end
+
+  defp print_red(msg) do
+     IO.puts IO.ANSI.format([:red, :bright, msg], true)
+  end
+  defp print_green(msg) do
+     IO.puts IO.ANSI.format([:green, :bright, msg], true)
+  end
+end
+
+defmodule HellOfAlotOfMonsters do
+  def release_em_all do
+    (1..100)
+      |> Enum.map(fn _x-> create_monster() end)
+      |> Enum.map(fn monster -> feed_monster monster end)
+  end
+
+  def create_monster do
+    spawn_link fn -> CookieMonster.read_cookie_monster_mailbox end
+  end
+
+  def feed_monster(monster) do
+    food = Enum.random ["cookie", "broccoli", nil, "orange", "banana"]
+    send monster, {:food, food}
   end
 end
 
@@ -26,7 +50,8 @@ end
 #   iex
 #   c "demo.exs"
 #   cookie_monster = spawn_link fn -> CookieMonster.read_cookie_monster_mailbox  end
-#   send cookie_monster, {:food, "nil"}
+#   send cookie_monster, {:food, nil}
 #   send cookie_monster, {:food, "cookie"}
 #   send cookie_monster, {:food, "broccoli"}
 #   send cookie_monster, {:garbge, "all kind of crap"}
+#   HellOfAlotOfMonsters.release_em_all()
